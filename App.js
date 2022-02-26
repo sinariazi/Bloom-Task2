30f4e6319be244f4a403fa0001460c999797efc2
Sina Riazi <52413397+sinariazi@users.noreply.github.com>
2022-02-26 16:38:51 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-//Task: Find a neighbouring subarray of the given sum
-//A list of positive integer
-// Input:
-// let array = [5, 4, 3, 9, 7, 2, 4, 61, 7, 88]
-// let targetSum = 16
-// Output: 
-// range (start-index, length)
 function subArraySum(inputArr, desiredSum)
 {
-	let curr_sum = inputArr[0], start = 0;
+	let currentSum = inputArr[0], start = 0;
   let length = inputArr.length;
+
 	for (let i = 1; i <= length; i++)
 	{
-		while (curr_sum > desiredSum && start < i - 1)
+		while (currentSum > desiredSum && start < i - 1)
 		{
-			curr_sum = curr_sum - inputArr[start];
+			currentSum = currentSum - inputArr[start];
 			start++;
 		}
-		if (curr_sum == desiredSum)
+		if (currentSum == desiredSum)
 		{
 			console.log("("+ start + ", " + inputArr.slice(start,i).length +")" +" -> ["+inputArr.slice(start,i)+"]");
 			return 
 		}
 		if (i < length)
-			curr_sum = curr_sum + inputArr[i];
+			currentSum = currentSum + inputArr[i];
 	}
 	console.log("No subarray equal to desiredSum has been found");
 	return 
